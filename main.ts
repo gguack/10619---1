6903dcd118c4b9196b439cfa4b5f0e605a2682f4
gguack <70557633+gguack@users.noreply.github.com>
2020-09-15 10:50:19 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("10619")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("oh wonjun")
+})
+basic.forever(function () {
+	
+})
